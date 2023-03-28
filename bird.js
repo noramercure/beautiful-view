6fe79ebcb2d75a42cd1fae5e779b4a5bd8894798
noramercure <129200168+noramercure@users.noreply.github.com>
2023-03-28 13:47:39 -0400

    Update bird.js

--- bird.js
+++ bird.js
@@ -3,19 +3,41 @@
 // Web amd Fx: from theory to practice, 502-A22, sect. 03
 //The Sunset
 
-
+let r = 255
+let g = 179
+let b = 0
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  back1 = loadImage("bakcground.jpeg")
-  bird = loadImage("birds.png")
+	createCanvas(windowWidth, windowHeight);
+	bg = loadImage("bakcground.jpeg")
+	bird = loadImage("birds.png")
+	night = loadImage("night.png")
+
+
+
 }
 
 function draw() {
-  // background first
-background(back1);
-  
-  image(bird, mouseX,150,40,40);
-fill(255,179,0)
-  circle(width/1.5, mouseX, 80)
+	// background first
+background(50);
+	image(bg,0,0,windowWidth,windowHeight)
+	image(bird, mouseX,150,100,100);
+	fill(r,g,b)
+	circle(width/1.5, mouseX, 150)
 }
+
+function keyPressed(){
+	if(key === 'n') {
+		bg = night
+	}
+}
+
+function mouseClicked(){
+	r = 255
+	g = 255
+	b = 255
+	
+}
+
+
+
